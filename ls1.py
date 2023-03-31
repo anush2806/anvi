108e0a11733b0be24f0406199036478535b210b0
Anoush <anush@mail.com>
2023-03-31 20:50:03 +0400

    added homework

--- ls1.py
+++ ls1.py
@@ -1,4 +1,4 @@
-def neg_ls(ls, n, m):
+def neg_ls(ls):
     return [(-x) if 3 <= x <= 8 else x for x in ls]
 
-print(neg_ls([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2, 8))
+print(neg_ls([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
